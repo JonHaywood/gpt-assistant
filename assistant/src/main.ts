65a0ab6c7695ade8f12008aa3c0f9ed3e3f2d4e9
jhaywood <jonathan.haywood@concentrix.com>
2024-09-20 16:13:56 -0600

    moved recognizer stuff into listener, put transcription stuff in recoginizer. trascribe works!

--- assistant/src/main.ts
+++ assistant/src/main.ts
@@ -1,17 +1,21 @@
 import OpenAI from 'openai';
-import * as Recognizer from './recognizer';
+import { listen } from './listener';
+import { recognize } from './recognizer';
 import fs from 'fs';
 
 async function main() {
   console.log('Starting listening...');
 
   try {
-    const audioBuffer = await Recognizer.listen({
+    const audioBuffer = await listen({
       silenceDuration: 2,
       phraseTimeLimit: 5,
     });
     console.log(`Received audio buffer of length ${audioBuffer.length}`);
-    fs.writeFileSync('audio.raw', audioBuffer);
+    //fs.writeFileSync('audio.raw', audioBuffer);
+
+    const text = await recognize(audioBuffer);
+    console.log('Transcription:', text);
   } catch (error) {
     console.error('Error listening:', error);
   }
