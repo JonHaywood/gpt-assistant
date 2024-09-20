5465fb0385f614d45cdf5162013320da543e71c9
jhaywood <jonathan.haywood@concentrix.com>
2024-09-20 13:37:47 -0600

    got listen function working, tweaked docker files and required packages

--- assistant/src/main.ts
+++ assistant/src/main.ts
@@ -1,37 +1,20 @@
 import OpenAI from 'openai';
-// @ts-ignore
-import * as recorder from 'node-record-lpcm16';
-import { PassThrough } from 'stream';
-import { createWriteStream } from 'fs';
-
-function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+import * as Recognizer from './recognizer';
+import fs from 'fs';
 
 async function main() {
-  const file = createWriteStream('test.wav', { encoding: 'binary' });
+  console.log('Starting listening...');
 
-  // Start recording audio and pipe it to the stream
-  const recording = recorder.record({
-    device: 'hw:1,0',
-    recorder: 'arecord', // Using arecord for ALSA
-  });
-
-  recording
-    .stream()
-    .on('error', (err: any) => {
-      console.error('recorder threw an error:', err);
-    })
-    .pipe(file);
-
-  console.log('Recording started...');
-
-  await wait(5000); // Wait for 5 seconds
-
-  // Stop recording audio
-  recording.stop();
-
-  console.log('Recording stopped.');
+  try {
+    const audioBuffer = await Recognizer.listen({
+      silenceDuration: 2,
+      phraseTimeLimit: 5,
+    });
+    console.log(`Received audio buffer of length ${audioBuffer.length}`);
+    fs.writeFileSync('audio.raw', audioBuffer);
+  } catch (error) {
+    console.error('Error listening:', error);
+  }
 }
 
 main();
