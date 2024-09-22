a220b0091b47d801d35cfcf224fdbe0f1101d498
jhaywood <jonathan.haywood@concentrix.com>
2024-09-22 11:53:39 -0600

    changed name to jarvis

--- assistant/src/ask.ts
+++ assistant/src/ask.ts
@@ -4,12 +4,14 @@ import { openai, OpenAIModels } from './openai';
 const logger = parentLogger.child({ filename: 'ask' });
 
 // TODO: pull from settings
-const ASSISTANT_NAME = 'Winston';
+const ASSISTANT_NAME = 'Jarvis';
 const SYSTEM_PROMPT = `
-  You are a friendly and helpful home assistant named ${ASSISTANT_NAME}. Your responses should
-  be clear, conversational, and easy to understand when spoken aloud. Always aim to be concise
-  and avoid long, complex sentences. Be polite, approachable, and positive in your tone. When
-  providing information, offer it in small, digestible pieces to avoid overwhelming the listener.
+  You are a friendly and helpful british home assistant named ${ASSISTANT_NAME}.
+
+  Your responses should be clear, conversational, and easy to understand when spoken aloud.
+  Always aim to be concise and avoid long, complex sentences. Be polite, approachable, and
+  positive in your tone. When providing information, offer it in small, digestible pieces
+  to avoid overwhelming the listener.
 
   When asked a question or given a command, respond in a way that feels natural for voice
   interactions. If the user asks for clarification or more details, feel free to elaborate.
@@ -24,9 +26,16 @@ const SYSTEM_PROMPT = `
   upbeat and engaging.
 `;
 
+// number of chat messages to keep in history
+const HISTORY_SIZE = 3;
+
+// history of chat messages.
+const chatHistory: { role: 'user' | 'assistant'; content: string }[] = [];
+
 export async function askAssistant(question: string): Promise<string> {
   try {
     logger.trace(`❔ Asking ChatGPT: ${question}`);
+
     const completion = await openai.chat.completions.create({
       model: OpenAIModels['4o'], // TODO: pull from settings
       messages: [
@@ -34,6 +43,7 @@ export async function askAssistant(question: string): Promise<string> {
           role: 'system',
           content: SYSTEM_PROMPT,
         },
+        ...chatHistory,
         {
           role: 'user',
           content: question,
@@ -47,6 +57,13 @@ export async function askAssistant(question: string): Promise<string> {
 
     // if no response something went wrong
     if (!response) throw new Error('No response from ChatGPT.');
+
+    // add the user question and assistant response to the chat history
+    chatHistory.push({ role: 'user', content: question });
+    chatHistory.push({ role: 'assistant', content: response });
+
+    // keep chat history within the limit
+    if (chatHistory.length > HISTORY_SIZE * 2) chatHistory.shift();
 
     return response;
   } catch (error) {
